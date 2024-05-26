7906ec650e9e1af9c0a09bbb82b8d7b47c162ad7
c0nD <colintiller21@gmail.com>
2024-05-26 01:23:22 -0400

    added Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+// Footer.tsx
+import React from "react";
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="footer">
+      made for ellery, with love ♡ -
+      <a
+        href="https://github.com/c0nD"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        colin
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
